cf014222a39166911e231d44ffa164b16fe8ae0b
kimdonwon <ehddnjs0728@naver.com>
2018-10-20 00:26:33 +0900

    test1

---
+++ router/MainControl.js
@@ -0,0 +1,12 @@
+module.exports = function(app){
+
+  app.get('/',function(req,res){
+      res.render('index');
+
+  })
+  app.get('/profile',function(req,res){
+
+    res.render('profile')
+  })
+
+}
---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
+var session = require('express-session');
+//var mongoose = require('mongoose');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
+app.use(session({
+  secret:'#$%^sad%$@',
+  resave:false,
+  saveUninitialized:true
+}));
+app.use(express.static('public'));
+
+var port = process.env.PORT || 80;
+
+var router = require('./router/MainControl')(app);
+
+var server = app.listen(port, function(){
+  console.log("gogo server " + port);
+})
+//anjdidlrjasdasdxzc
